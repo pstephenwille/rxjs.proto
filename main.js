3e7b44deabd5c7c408cac87d38d3007839ba59b6
stephen wille <stephen.home@ubu.com>
2015-05-31 10:49:36 -0700

    promises vs observables

--- main.js
+++ main.js
@@ -1,8 +1,40 @@
-//var source = Rx.Observable.interval(500).take(6);
-var source = Rx.Observable.fromArray([1, 2, 3, 4, 5]);
+console.clear();
 
+/*
+ * promise always runs */
+var promise = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log('promise timeout hit');
+    resolve(42);
+  }, 1000);
+  console.log('promise started');
+});
 
+promise.then(x => console.log(x));
 
-source.filter(x => x % 2 === 1)
-  .map(x => x + '!')
-  .forEach(x => console.log(x));
+/*
+ * observables are lazy.
+ * contain thier own setup/tear down code blocks.
+ * references to observables allow you to re-call it, and get fresh results.
+ * traditional promises return stale results.
+ * */
+var source = Rx.Observable.create((observer) => {
+  var id = setTimeout(() => {
+    /* set up code */
+    console.log('observable timeout hit');
+    observer.onNext([33]);
+  }, 1000);
+  console.log('observable started');
+
+  return () => {
+    /* tear down & clean up code */
+    console.log('dispose called');
+    clearTimeout(id);
+  };
+});
+
+var disposable = source.forEach(x => console.log(x));
+
+setTimeout(() => {
+  disposable.dispose();
+}, 500);
